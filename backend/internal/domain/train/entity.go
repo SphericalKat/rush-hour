58e3fea910b0e323c5dee543aeaae82bdff313ec
SphericalKat <me@kat.bio>
2026-02-28 22:53:36 +0530

    add origin and destination to departures API

--- backend/internal/domain/train/entity.go
+++ backend/internal/domain/train/entity.go
@@ -1,11 +1,13 @@
 package train
 
 type Departure struct {
-	Number    string `db:"number"`
-	Code      string `db:"code"`
-	IsAC      bool   `db:"is_ac"`
-	Direction string `db:"direction"`
-	Line      string `db:"line"`
-	Departure int    `db:"departure"`
-	Station   string `db:"station"`
+	Number      string `db:"number"`
+	Code        string `db:"code"`
+	IsAC        bool   `db:"is_ac"`
+	Direction   string `db:"direction"`
+	Line        string `db:"line"`
+	Departure   int    `db:"departure"`
+	Station     string `db:"station"`
+	Origin      string `db:"origin"`
+	Destination string `db:"destination"`
 }
